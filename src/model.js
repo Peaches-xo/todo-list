2e17a8e5cd2963fcb85a5aa400eb98b9e9f6967c
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2023-08-25 17:36:44 +1000

    updated modal

--- src/model.js
+++ src/model.js
@@ -5,34 +5,34 @@ function modelfunc(){
     console.log('modelfunc called');
 
 
-    //array of all projects
-    let allProjectsArr = [];
+    // //array of all projects
+    // let allProjectsArr = [];
 
 
-    class Project {
-        constructor(name, icon){ //remove icon til ready to add
-            this.name = name,
-            //this.icon = icon
-        }
-        tasks = [];
-        createProj(){
-            //creates new project and put in projects array
-        }
-        deleteProj(){
-            //modal pop up 'are you sure' if yes,
-            //find proj in projs array and deletes. proj arrray is updated.
-        }
-    }
+    // class Project {
+    //     constructor(name){ //remove icon til ready to add
+    //         this.name = name,
+    //         //this.icon = icon
+    //     }
+    //     tasks = [];
+    //     createProj(){
+    //         //creates new project and put in projects array
+    //     }
+    //     deleteProj(){
+    //         //modal pop up 'are you sure' if yes,
+    //         //find proj in projs array and deletes. proj arrray is updated.
+    //     }
+    // }
 
-    let defaultproj = new Project('default');
+    // let defaultproj = new Project('default');
 
-    class Task {
-        constructor(name, description, project, priority, duedate){
-            this.name = name,
-            this.icon = icon,
-            this.tasks = tasks
-        }
-    }
+    // class Task {
+    //     constructor(name, description, project, priority, duedate){
+    //         this.name = name,
+    //         this.icon = icon,
+    //         this.tasks = tasks
+    //     }
+    // }
 
 
 
